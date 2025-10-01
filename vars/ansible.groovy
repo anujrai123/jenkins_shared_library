5d02acfbc801d442d95656f1289ebad0e006c832
Anuj Rai <43175782+anujrai123@users.noreply.github.com>
2025-10-01 18:05:53 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -1,6 +1,6 @@
 def call(String playbook, String server, String action) {
     sh """
-        ssh -i $SSH_KEY -o StrictHostKeyChecking=no ubuntu@$3.108.196.80 \
+        ssh -i $SSH_KEY -o StrictHostKeyChecking=no ubuntu@3.108.196.80 \
         "ansible-playbook -i '${server},' /home/ubuntu/Ansible/playbook/${playbook} --extra-vars 'action=${action}'"
     """
 }
